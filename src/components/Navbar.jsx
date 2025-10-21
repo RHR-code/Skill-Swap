bfd6d9b8b253efc6555c83b6b78cd8427de14dca
RHR-code <{ID}+{username}@users.noreply.github.com>
2025-10-21 20:06:48 +0600

    theme added and changed the website from dark them to light

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -71,7 +71,7 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a className="btn btn-primary">Button</a>
       </div>
     </div>
   );
